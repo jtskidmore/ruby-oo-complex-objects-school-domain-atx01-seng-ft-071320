9979eec2c69ecc57edfb43aa3531f52e452fc778
JT Skidmore <johntoddskidmore@gmail.com>
2020-07-15 03:45:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -24,9 +24,13 @@ class School
     @roster[grade]
   end
 
+
   def sort
     @roster = @roster.sort.to_h
     @roster.each do |key, value|
+#you cannot change @roster by trying to call methods on KEY and VALUE
+#KEY and VALUE merely reference properties of @roster
+#use KEY and VALUE as information that then can be applied
       @roster[key] = value.sort
     end
   end
