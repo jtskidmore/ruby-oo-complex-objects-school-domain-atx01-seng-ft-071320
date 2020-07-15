5ccb23f1c0784c9366dfefd8f9689055196af24a
JT Skidmore <johntoddskidmore@gmail.com>
2020-07-15 02:05:56 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,14 @@
 # code here!
+
+class School
+
+  def initialize (name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student (student, grade)
+    @roster[student] << grade
+  end
+  
+end
