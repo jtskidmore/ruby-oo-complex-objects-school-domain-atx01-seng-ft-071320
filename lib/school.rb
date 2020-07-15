430d162825e625b5e13fc94ec9b62669ac6f194b
JT Skidmore <johntoddskidmore@gmail.com>
2020-07-15 03:22:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,11 +12,11 @@ class School
   end
 
   def add_student (student, grade)
-    if hash[grade.to_i] == nil
-      hash[grade.to_i] = []
-      hash[grade.to_i] << "student"
-    elsif hash[grade.to_i] != nil
-      hash[grade.to_i] << student
+    if @roster[grade.to_i] == nil
+      @roster[grade.to_i] = []
+      @roster[grade.to_i] << "student"
+    elsif @roster[grade.to_i] != nil
+      @roster[grade.to_i] << student
     end
   end
 
